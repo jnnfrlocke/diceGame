48b4dec0127e5ff822fb7450f11cb5c65be1045b
Jen <jnnfrlocke@gmail.com>
2017-08-23 07:22:46 -0500

    Complete 4 sided dice roll

---
+++ dice_game.js
@@ -0,0 +1,8 @@
+// A game that uses at least one 4-sided, 6-sided, 8-sided, 10-sided, 12-sided, and 20-sided dice
+
+function roll4Sided() {
+    var roll4 = (Math.round(Math.random() * (4 - 1) + 1));
+    console.log(roll4);
+}
+
+roll4Sided();
