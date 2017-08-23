4d53545dde0d7cd1c6b10f863a799dbf79866929
Jen <jnnfrlocke@gmail.com>
2017-08-23 08:46:59 -0500

    Add switch/case statements for 12 sided roll.

--- dice_game.js
+++ dice_game.js
@@ -62,50 +62,89 @@
 // roll8Sided();
 
 
-function roll10Sided() {
-    var roll10 = (Math.round(Math.random() * (10 - 1) + 1));
-    switch (roll10) {
-        case 1:
-            console.log("You will eat home cooking tonight.");
-            break;
-        case 2:
-            console.log("You will eat southern food tonight.");
-            break;
-        case 3:
-            console.log("You will eat Mexican tonight.");
-            break;
-        case 4:
-            console.log("You will eat Mexican food tonight.");
-            break;
-        case 5:
-            console.log("You will have French cuisine tonight.");
-            break;
-        case 6:
-            console.log("You will have Greek food tonight.");
-            break;
-        case 7:
-            console.log("You will eat Persian food tonight.");
-            break;
-        case 8:
-            console.log("You will eat Chinese food tonight.");
-            break;
-        case 9:
-            console.log("You will eat German food tonight.");
-            break;
-        default:
-            console.log("You are having Japanese food tonight.");
-    }
-    // console.log(roll10);
-}
-
-roll10Sided();
-
-// function roll12Sided() {
-//     var roll12 = (Math.round(Math.random() * (12 - 1) + 1));
-//     console.log(roll12);
+// function roll10Sided() {
+//     var roll10 = (Math.round(Math.random() * (10 - 1) + 1));
+//     switch (roll10) {
+//         case 1:
+//             console.log("You will eat home cooking tonight.");
+//             break;
+//         case 2:
+//             console.log("You will eat southern food tonight.");
+//             break;
+//         case 3:
+//             console.log("You will eat Mexican tonight.");
+//             break;
+//         case 4:
+//             console.log("You will eat Mexican food tonight.");
+//             break;
+//         case 5:
+//             console.log("You will have French cuisine tonight.");
+//             break;
+//         case 6:
+//             console.log("You will have Greek food tonight.");
+//             break;
+//         case 7:
+//             console.log("You will eat Persian food tonight.");
+//             break;
+//         case 8:
+//             console.log("You will eat Chinese food tonight.");
+//             break;
+//         case 9:
+//             console.log("You will eat German food tonight.");
+//             break;
+//         default:
+//             console.log("You are having Japanese food tonight.");
+//     }
+//     // console.log(roll10);
 // }
 
-// roll12Sided();
+// roll10Sided();
+
+
+function roll12Sided() {
+    var roll12 = (Math.round(Math.random() * (12 - 1) + 1));
+    switch (roll12) {
+        case 1:
+            console.log("Your primary mode of transportation will be walking.");
+            break;
+        case 2:
+            console.log("You will do a lot of driving on your trip.");
+            break;
+        case 3:
+            console.log("You'll primarily paddle in your canoe while on vacation.");
+            break;
+        case 4:
+            console.log("You're going to spend a lot of time in your kayak.");
+            break;
+        case 5:
+            console.log("You will use the local bus system a lot.");
+            break;
+        case 6:
+            console.log("You're going to ride the train a lot.");
+            break;
+        case 7:
+            console.log("You will ride a horse to get around.");
+            break;
+        case 8:
+            console.log("Do you know how to ride a camel? You will learn and use it as your primary mode of transportation.");
+            break;
+        case 9:
+            console.log("You're going to ride a donkey around your destination.");
+            break;
+        case 10:
+            console.log("You're gonna bike your way around your destination.");
+            break;
+        case 11:
+            console.log("You'll be zooming around on a scooter during this trip.");
+            break;
+        default:
+            console.log("Are you in India? Because you're going to b taking rickshaws a lot.");
+    }
+    // console.log(roll12);
+}
+
+roll12Sided();
+
 
 // function roll20Sided() {
 //     var roll20 = (Math.round(Math.random() * (20 - 1) + 1));
