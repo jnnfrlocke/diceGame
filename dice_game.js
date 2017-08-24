3237ae9809baaa6e1b32f55f93b4918a32e095e8
Jen <jnnfrlocke@gmail.com>
2017-08-24 14:45:01 -0500

    Update answers

--- dice_game.js
+++ dice_game.js
@@ -70,6 +70,7 @@ function hideQuestion6() {
     fs();
 }
 
+// Return to the first page
 function restart() {
     document.getElementById("end").style.display = "none";
     document.getElementById("question1").style.display = "block";
@@ -77,6 +78,7 @@ function restart() {
     location.reload();
 }
 
+// Final page
 function finalStatement(finalResult) {
     finalState.push(finalResult);
     if (finalState.length === 7) {
@@ -89,17 +91,18 @@ function finalStatement(finalResult) {
     }
 }
 
+// Roll dice
 function roll4Sided() {
     var roll4 = (Math.round(Math.random() * (4 - 1) + 1));
     var answer = "";
     if (roll4 === 1) {
-        answer = "You'll be visiting a location by the sea.";
+        answer = "You'll be dreaming by the sea.";
     } else if (roll4 === 2) {
-        answer = "You're going to a place in the woods.";
+        answer = "You're going to relax in the woods.";
     } else if (roll4 === 3) {
-        answer = "You are going to the city.";
+        answer = "You are going to party in the city.";
     } else {
-        answer = "You'll be on a tropical island.";
+        answer = "You'll be chilling in the sun on a tropical island.";
     }
     console.log(answer);
     return answer;
@@ -111,15 +114,15 @@ function roll6Sided() {
     if (roll6 === 1) {
         answer2 = "You'll be staying in a hotel.";
     } else if (roll6 === 2) {
-        answer2 = "You'll be staying at an all-inclusive resort.";
+        answer2 = "You'll be staying at a posh all-inclusive resort.";
     } else if (roll6 === 3) {
         answer2 = "You will stay at a cozy bed & breakfast.";
     } else if (roll6 === 4) {
-        answer2 = "You're going to be sleeping in a tent.";
+        answer2 = "You're going to be roughing it in a tent.";
     } else if (roll6 === 5) {
-        answer2 = "You will stay on a house boat.";
+        answer2 = "You will float in the harbor on a house boat.";
     } else {
-        answer2 = "You will stay with friends and family.";
+        answer2 = "You'll bunk with friends and family.";
     }
     console.log(answer2);
     return answer2;
@@ -129,21 +132,21 @@ function roll8Sided() {
     var roll8 = (Math.round(Math.random() * (8 - 1) + 1));
     var answer3 = "";
     if (roll8 === 1) {
-        answer3 = "You will forget your toothbrush.";
+        answer3 = "You'll forget your toothbrush.";
     } else if (roll8 === 2) {
-        answer3 = "You will forget toothpaste.";
+        answer3 = "You'll forget toothpaste.";
     } else if (roll8 === 3) {
-        answer3 = "You will forget your comb.";
+        answer3 = "You'll forget your comb.";
     } else if (roll8 === 4) {
-        answer3 = "You will forget soap.";
+        answer3 = "You'll forget soap. Are you at a hotel? Because they typically supply soap - lucky you!";
     } else if (roll8 === 5) {
-        answer3 = "You will forget your underwear.";
+        answer3 = "You will forget your underwear. Oops! Gotta run to the store!";
     } else if (roll8 === 6) {
-        answer3 = "You will forget sunscreen.";
+        answer3 = "You will forget sunscreen. Who cares about a little sunburn, anyway? You will if you get skin cancer...";
     } else if (roll8 === 7) {
-        answer3 = "You will forget bug spray.";
+        answer3 = "You will forget bug spray. Don't let those skeeters get ya!";
     } else {
-        answer3 = "You will forget some of your luggage.";
+        answer3 = "Really? You'll forget your luggage? Maybe someone from home can FedEx it to you?";
     }
     console.log(answer3);
     return answer3;
@@ -154,34 +157,33 @@ function roll10Sided() {
     var answer4 = "";
     switch (roll10) {
         case 1:
-            answer4 = "You will eat home cooking tonight.";
+            answer4 = "You will eat home cooking tonight. Mmmm... comfort food.";
             break;
         case 2:
-            answer4 = "You will eat southern food tonight.";
+            answer4 = "You will eat southern food tonight. Y'all better go up the road a piece to find it.";
             break;
         case 3:
-            answer4 = "You will eat Mexican tonight.";
+            answer4 = "You will eat Mexican tonight. Bueno, no?";
             break;
         case 4:
-            answer4 = "You will eat Mexican food tonight.";
+            answer4 = "You will have French cuisine tonight. Bon appetit!";
             break;
         case 5:
-            answer4 = "You will have French cuisine tonight.";
+            answer4 = "You will have Greek food tonight. Opaa!";
             break;
         case 6:
-            answer4 = "You will have Greek food tonight.";
+            answer4 = "You will eat Persian food tonight. Beh salamati";
             break;
         case 7:
-            answer4 = "You will eat Persian food tonight.";
+            answer4 = "You will eat Chinese food tonight. Ganbei!";
             break;
         case 8:
-            answer4 = "You will eat Chinese food tonight.";
+            answer4 = "You will eat German food tonight. Enjoy a great German beer with it!";
             break;
         case 9:
-            answer4 = "You will eat German food tonight.";
-            break;
+            answer4 = "You're going to enjoy Italian food tonight. Have a fine wine with your meal!"
         default:
-            answer4 = "You are having Japanese food tonight.";
+            answer4 = "You are having Japanese food tonight. Indulge in some saki! But avoid the fugu...";
     }
     console.log(answer4);
     return answer4;
@@ -192,34 +194,34 @@ function roll12Sided() {
     var answer5 = "";
     switch (roll12) {
         case 1:
-            answer5 = "Your primary mode of transportation will be walking.";
+            answer5 = "Your primary mode of transportation will be walking. Get those walking shoes ready.";
             break;
         case 2:
-            answer5 = "You will do a lot of driving on your trip.";
+            answer5 = "You will do a lot of driving on your trip. Do you need driving gloves?";
             break;
         case 3:
-            answer5 = "You'll primarily paddle in your canoe while on vacation.";
+            answer5 = "You'll primarily paddle in your canoe while on vacation. Do you have a companion to share paddling duties?";
             break;
         case 4:
-            answer5 = "You're going to spend a lot of time in your kayak.";
+            answer5 = "You're going to spend a lot of time in your kayak. Don't forget your PFD!";
             break;
         case 5:
-            answer5 = "You will use the local bus system a lot.";
+            answer5 = "You will use the local bus system a lot. Get that bus pass!";
             break;
         case 6:
-            answer5 = "You're going to ride the train a lot.";
+            answer5 = "You're going to ride the train a lot. Do you have the local publice transportation app?";
             break;
         case 7:
-            answer5 = "You will ride a horse to get around.";
+            answer5 = "You will ride a horse to get around. Chaps anyone?";
             break;
         case 8:
             answer5 = "Do you know how to ride a camel? You will learn and use it as your primary mode of transportation.";
             break;
         case 9:
-            answer5 = "You're going to ride a donkey around your destination.";
+            answer5 = "You're going to ride a donkey around your destination. Are you at the Grand Canyon?!";
             break;
         case 10:
-            answer5 = "You're gonna bike your way around your destination.";
+            answer5 = "You're gonna bike your way around your destination. They don't allow cars? You might be on Mackinac Island.";
             break;
         case 11:
             answer5 = "You'll be zooming around on a scooter during this trip.";
@@ -236,37 +238,37 @@ function roll20Sided() {
     var answer6 = "";
     switch (roll20) {
         case 1:
-            answer6 = "You're going to the carnival today!";
+            answer6 = "You're going to the carnival today! Cotton candy? Amusement rides? Yes, yes!";
             break;
         case 2:
-            answer6 = "You're going to the zoo today!";
+            answer6 = "You're going to the zoo today! Lions and tigers and bears, oh my!";
             break;
         case 3:
-            answer6 = "You're going swimming today!";
+            answer6 = "You're going swimming today! Hopefully you didn't forget your suit.";
             break;
         case 4:
-            answer6 = "You're going to enjoy some time on the water in a boat today!";
+            answer6 = "You're going to enjoy some time on the water in a boat today! Bring a jacket - it might get breezy!";
             break;
         case 5:
-            answer6 = "You're going to take a hike today!";
+            answer6 = "You're going to take a hike today! Get out your gear.";
             break;
         case 6:
-            answer6 = "You're going fishing today!";
+            answer6 = "You're going fishing today! Got your rod and line?";
             break;
         case 7:
-            answer6 = "You're going to a water park today!";
+            answer6 = "You're going to a water park today! Don't overdo it.";
             break;
         case 8:
-            answer6 = "You're going to visit the casino today!";
+            answer6 = "You're going to visit the casino today! Don't roll snake eyes!";
             break;
         case 9:
-            answer6 = "You're visiting the racetrack today!";
+            answer6 = "You're visiting the racetrack today! You might want some earplugs. Those engines are LOUD!";
             break;
         case 10:
-            answer6 = "You're going to watch the sunset tonight!";
+            answer6 = "You're going to watch the sunset tonight! You're such a dreamer. ";
             break;
         case 11:
-            answer6 = "You're going to a concert tonight!";
+            answer6 = "You're going to a concert tonight! Don't get injured by bodysurfing.";
             break;
         case 12:
             answer6 = "You will watch a play tonight! Perhaps something by Tennessee Williams?";
